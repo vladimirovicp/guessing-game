0221dbfb2b5dac9cf560c81c35c0deceab6b735e
vladimirovicp <vladimirovicp70@gmail.com>
2022-08-08 20:31:45 +0400

    feat: final

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
 
-    setRange(min, max) {
+    constructor() {
 
     }
 
+    setRange(min, max) {
+    //этот метод принимает минимальное и максимальное значение диапазона чисел для угадывания
+        this.min = min;
+        this.max = max;
+    }
     guess() {
-
+    //этот метод возвращает кандидата решения (вы делаете предположение на основе диапазона и предыдущих предположений)
+        return Math.round(((this.max - this.min)/2) + this.min);
     }
 
     lower() {
-
+    //этот метод вызывается, если предыдущий вызов guess() вернул число, которое больше, чем ответ
+        this.max = Math.round(((this.max - this.min)/2) + this.min);
     }
 
     greater() {
-
+        this.min = Math.round(((this.max - this.min)/2) + this.min);
+    //этот метод вызывается, если предыдущий вызов функции guess() вернул число, которое меньше, чем ответ
     }
 }
 
